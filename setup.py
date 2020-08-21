36cbd31f9c52e733a94c4f913ee8f4535e452ab6
Or Levi <orlevi128@gmail.com>
2020-08-21 17:45:32 +0300

    chore: updated to use setup.cfg

--- setup.py
+++ setup.py
@@ -1,52 +1,13 @@
-import setuptools
-
 from setuptools import setup
-
-with open('README.md', 'r') as fh:
-    long_description = fh.read()
 
 # Pull version from source without importing
 # since we can't import something we haven't built yet :)
 exec(open('mlserving/__version__.py').read())
 
 setup(
-    name='mlserving',
-    packages=setuptools.find_packages(exclude=['tests*', 'examples*']),
-
-    # Project URL
-    url='https://github.com/orlevii/mlserving',
-
-    # Author
-    author='Or Levi',
-    author_email='orlevi128@gmail.com',
-    # Needed for dependencies
+    # Needed for dependency graph on GitHub
     install_requires=['falcon==2.*', 'validr==1.2.*'],
 
-    # Package version
+    # Package version - taken from code, cannot be in setup.cfg file
     version=__version__,
-    license='MIT',
-    description='A framework for developing a realtime model-inference service.',
-    data_files=[('', ['README.md'])],
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
-    classifiers=['Development Status :: 3 - Alpha',
-                 'Environment :: Web Environment',
-                 'Intended Audience :: Developers',
-                 'License :: OSI Approved :: MIT License',
-                 'Operating System :: MacOS :: MacOS X',
-                 'Operating System :: POSIX',
-                 'Operating System :: OS Independent',
-                 'Programming Language :: Python',
-                 'Programming Language :: Python :: 3',
-                 'Programming Language :: Python :: 3.6',
-                 'Programming Language :: Python :: 3.7',
-                 'Programming Language :: Python :: 3.8',
-                 'Programming Language :: Python :: 3 :: Only',
-                 'Topic :: Internet',
-                 'Topic :: Utilities',
-                 'Topic :: Software Development :: Libraries :: Python Modules'
-                 ],
-
-    long_description_content_type='text/markdown',
-    python_requires='>=3.6',
-    long_description=long_description
 )
